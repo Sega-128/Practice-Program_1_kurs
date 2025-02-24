2f2e9f38db99e330dac8921f2ca6a3b996e6f48d
sega-128 <sehinihor@uzhnu.edu.ua>
2025-02-24 09:50:15 +0200

    example

--- practice_String_1/Program.cs
+++ practice_String_1/Program.cs
@@ -3,12 +3,42 @@ Console.WriteLine("Hello, World!");
 
 
 string str1 = "Hello";
-string str2 = "World";
-string result = str1 + " " + str2;
-Console.WriteLine(result);
+string str2 = "Hello";
+string str3 = "World";
+
+bool result1 = str1 == str2; // true
+bool result2 = str1 == str3; // false
+
+Console.WriteLine(result1); // true
+Console.WriteLine(result2); // false
+
 
 
 //string str1 = "Hello";
-//string str2 = "World";
-//string result = $"{str1} {str2}";
-//Console.WriteLine(result);
+//string str2 = "Hello";
+//string str3 = "World";
+
+//bool result1 = str1.Equals(str2); // true
+//bool result2 = str1.Equals(str3); // false
+
+//Console.WriteLine(result1); // true
+//Console.WriteLine(result2); // false
+
+
+//string str1 = "hello";
+//string str2 = "HELLO";
+
+//bool result = str1.Equals(str2, StringComparison.OrdinalIgnoreCase); // true
+//Console.WriteLine(result); // true
+
+//string str1 = "apple";
+//string str2 = "banana";
+//string str3 = "apple";
+
+//int comparison1 = string.Compare(str1, str2); // <0
+//int comparison2 = string.Compare(str1, str3); // 0
+//int comparison3 = str1.CompareTo(str2); // <0
+
+//Console.WriteLine(comparison1); // < 0
+//Console.WriteLine(comparison2); // 0
+//Console.WriteLine(comparison3); // < 0
