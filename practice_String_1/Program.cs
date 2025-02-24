3ad82487885bc8ea9cd9bbe30e667e4194991448
sega-128 <sehinihor@uzhnu.edu.ua>
2025-02-24 12:53:55 +0200

    example link arr and str

--- practice_String_1/Program.cs
+++ practice_String_1/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int[] arr1 = { 1, 2, 3 };
+
+int[] arr2 = arr1;
+
+arr2[0] = 100;
+
+Console.WriteLine(arr1[0]);
+
+//string str1 = "hello";
+
+//string str2 = str1;
+
+//str1 = "test";
+
+//Console.WriteLine(str2);
