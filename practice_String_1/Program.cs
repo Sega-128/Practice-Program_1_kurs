c29d5502523150d4f0f4127398bf665278ba2197
sega-128 <sehinihor@uzhnu.edu.ua>
2025-02-24 09:25:31 +0200

    example_enumerable_char

--- practice_String_1/Program.cs
+++ practice_String_1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// IEnumerable<char>
+string str = "Hello";
+char firstChar = str[0]; // 'H'
+char lastChar = str[str.Length - 1]; // 'o'
+
+Console.WriteLine($"First character: {firstChar}, Last character: {lastChar}");
+
+//foreach (char c in str)
+//{
+//    Console.WriteLine(c);
+//}
+
+
