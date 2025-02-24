278a12844ebb356607e7967e94fbaf2f99811828
sega-128 <sehinihor@uzhnu.edu.ua>
2025-02-24 09:35:18 +0200

    concatenation and interpolation

--- practice_String_1/Program.cs
+++ practice_String_1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+string str1 = "Hello";
+string str2 = "World";
+string result = str1 + " " + str2;
+Console.WriteLine(result);
+
+
+//string str1 = "Hello";
+//string str2 = "World";
+//string result = $"{str1} {str2}";
+//Console.WriteLine(result);
