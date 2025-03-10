eb2471d4260df220386f90d57f0eefb30276730a
sega-128 <sehinihor@uzhnu.edu.ua>
2025-03-10 06:35:04 +0200

    feat: task 1

--- practice_String_1/Program.cs
+++ practice_String_1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        BankAccountClass accountClass = new BankAccountClass(1000);
+        BankAccountClass accountClassCopy = accountClass;
+
+        BankAccountStruct accountStruct = new BankAccountStruct(1000);
+        BankAccountStruct accountStructCopy = accountStruct;
+
+        accountClass.Deposit(500);
+        accountStruct.Deposit(500);
+
+        Console.WriteLine($"Клас (оригінал): {accountClass.Balance}");
+        Console.WriteLine($"Клас (копія): {accountClassCopy.Balance}");
+        Console.WriteLine($"Структура (оригінал): {accountStruct.Balance}");
+        Console.WriteLine($"Структура (копія): {accountStructCopy.Balance}");
+
+        accountClass.Withdraw(200);
+        accountStruct.Withdraw(200);
+
+        Console.WriteLine($"Клас (оригінал): {accountClass.Balance}");
+        Console.WriteLine($"Клас (копія): {accountClassCopy.Balance}");
+        Console.WriteLine($"Структура (оригінал): {accountStruct.Balance}");
+        Console.WriteLine($"Структура (копія): {accountStructCopy.Balance}");
+    }
+}
+
