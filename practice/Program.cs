3660569770daded90370a7a090248837c6ffaa1b
Sega-128 <seginigor96@gmail.com>
2025-03-17 07:40:39 +0200

    Revert "feat: task 2"
    This reverts commit 954447e48e1017c1af0add65afb05e5e0b78e7e8.

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,14 +1,2 @@
-﻿using System;
-
-public delegate void Operation(int a, int b);
-
-class Program
-{
-    static void PrintSum(int a, int b) => Console.WriteLine(a + b);
-
-    static void Main()
-    {
-        Operation op = PrintSum;
-        Console.WriteLine($"Результат: {op(5, 3)}");
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
