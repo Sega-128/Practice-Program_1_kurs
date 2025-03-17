bfcfdd422186ef1a57593c15c0bddc9ca4367aa1
Sega-128 <seginigor96@gmail.com>
2025-03-17 05:18:19 +0200

    feat: example 2

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public delegate int Operation(int a, int b);
+
+class Program
+{
+    static int Add(int a, int b) => a + b;
+    static int Multiply(int a, int b) => a * b;
+
+    static void Calculate(int x, int y, Operation operation)
+    {
+        Console.WriteLine($"Результат: {operation(x, y)}");
+    }
+
+    static void Main()
+    {
+        Calculate(5, 3, Add);       
+        Calculate(5, 3, Multiply);
+    }
+}
