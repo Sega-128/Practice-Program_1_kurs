ca83ce5fc3b0b5c7542b38d5c55b6d31b08f55fe
Sega-128 <seginigor96@gmail.com>
2025-03-17 06:34:00 +0200

    feat: example delegate event

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public delegate void EventHandler(string message); 
+
+class EventSource
+{
+    public event EventHandler? OnChange; 
+
+    public void TriggerEvent(string message) 
+    {
+        OnChange?.Invoke(message);
+    }
+}
+
+class Program
+{
+    static void ShowMessage(string msg) => Console.WriteLine($"Отримано повідомлення: {msg}"); 
+
+    static void Main()
+    {
+        EventSource source = new EventSource();
+
+        source.OnChange += ShowMessage;
+
+        source.TriggerEvent("Привіт!"); 
+
+        source.OnChange -= ShowMessage; 
+    }
+}
