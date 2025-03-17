d05002be989313a6eeb3f6befc1146b29159de31
Sega-128 <seginigor96@gmail.com>
2025-03-17 07:25:31 +0200

    feat: task 1

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+
+class OrderProcessor
+{
+    public void ProcessOrder(string restaurant, double price)
+    {
+        double total;
+
+        if (restaurant == "McBurger")
+        {
+            total = price * 0.9; 
+            Console.WriteLine($"McBurger: Вартість замовлення після знижки: {total} грн");
+        }
+        else if (restaurant == "PizzaHouse")
+        {
+            total = price + 20; 
+            Console.WriteLine($"PizzaHouse: Вартість замовлення з доставкою: {total} грн");
+        }
+        else
+        {
+            total = price; 
+            Console.WriteLine($"Звичайний ресторан: Вартість замовлення: {total} грн");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        OrderProcessor processor = new OrderProcessor();
+        processor.ProcessOrder("McBurger", 200);
+        processor.ProcessOrder("PizzaHouse", 150);
+        processor.ProcessOrder("SushiBar", 300);
+    }
+}
