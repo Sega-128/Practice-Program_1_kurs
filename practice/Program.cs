004c90d364a74e332e8feef8d2a4ae0d7ca5c242
Sega-128 <seginigor96@gmail.com>
2025-03-17 06:08:44 +0200

    feat: example 4

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public delegate int Calculate();
+
+class Program
+{
+    static int GetTen() => 10;
+    static int GetTwenty() => 20;
+
+    static void Main()
+    {
+        Calculate calc = GetTen;
+        calc += GetTwenty;
+
+        int result = calc(); 
+        Console.WriteLine(result);
+    }
+}
