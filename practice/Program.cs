a210e7dadd304be88480442eee258f4659006264
Sega-128 <seginigor96@gmail.com>
2025-03-17 05:56:54 +0200

    feat: Multicast Delegates

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public delegate void Notify();
+
+class Program
+{
+    static void Method1() => Console.WriteLine("Method1 викликано!");
+    static void Method2() => Console.WriteLine("Method2 викликано!");
+
+    static void Main()
+    {
+        Notify notifier = Method1;
+        notifier += Method2;
+
+        Console.WriteLine("notifier call 1");
+        notifier();
+
+        notifier -= Method1;
+
+        Console.WriteLine("notifier call 2");
+        notifier();
+    }
+}
